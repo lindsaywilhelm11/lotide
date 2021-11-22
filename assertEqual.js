a98314977b23aeda8015d820ab049fc6aa2fb4c0
Lindsay Wilhelm <lindsaywilhelm11@gmail.com>
2021-11-22 22:11:33 +0000

    added template literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,11 +1,13 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("Passed: " + actual + " === " + expected);
+    console.log(`Passed:  ${actual}   ===  ${expected}`);
   } else if (actual !== expected) {
-    console.log("Assertion Failed: " + actual + " !== " + expected);
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
 // Test Code
+assertEqual(1, 1);
+assertEqual(2, 1);
 assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual("Hey", "Hi");
+assertEqual("Hi", "Hi");
