4bd8ebae5d88beefa58241ece5de0324e2d1d00a
lindsaywilhelm11 <lindsaywilhelm11@gmail.com>
2021-11-25 23:18:12 +0000

    added function to find the value by looping through an object and using a callback function

---
+++ findKey.js
@@ -0,0 +1,51 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Passed:  ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function(object, callback) {
+  let nameResult = "";
+  for (let name in object) {
+    if (callback(object[name])) {
+      nameResult = name;
+      return nameResult;
+      break; 
+    } 
+  } return undefined;
+}
+
+const result = findKey({
+    "Blue Hill": { stars: 1 },
+    "Akaleri":   { stars: 3 },
+    "noma":      { stars: 2 },
+    "elBulli":   { stars: 3 },
+    "Ora":       { stars: 2 },
+    "Akelarre":  { stars: 3 }
+  }, x => x.stars === 2) // => "noma"
+
+assertEqual(result, "noma")
+
+const result1 = findKey({
+    "Blue Hill": { stars: 1 },
+    "Akaleri":   { stars: 3 },
+    "noma":      { stars: 2 },
+    "elBulli":   { stars: 3 },
+    "Ora":       { stars: 2 },
+    "Akelarre":  { stars: 3 }
+  }, x => x.stars === 1)
+
+assertEqual(result1, "Blue Hill")
+
+const result2 = findKey({
+    "Blue Hill": { stars: 1 },
+    "Akaleri":   { stars: 3 },
+    "noma":      { stars: 2 },
+    "elBulli":   { stars: 3 },
+    "Ora":       { stars: 2 },
+    "Akelarre":  { stars: 3 }
+  }, x => x.stars !== 1 && x.stars !== 2) 
+
+assertEqual(result2, "Akaleri")
