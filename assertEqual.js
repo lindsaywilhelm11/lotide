09d8c8a004b463e94a0ef46804441ce19657c5d0
lindsaywilhelm11 <lindsaywilhelm11@gmail.com>
2021-11-25 02:07:56 +0000

    added check for assertObjectEqual

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`Passed:  ${actual} === ${expected}`);
+    console.log(`✅✅✅ Passed:  ${actual} === ${expected}`);
   } else if (actual !== expected) {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
