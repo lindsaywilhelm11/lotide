a66e5119692035847823ddb95f2a99b697c4640e
lindsaywilhelm11 <lindsaywilhelm11@gmail.com>
2021-11-29 15:20:23 +0000

    refactored assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,15 +1,4 @@
-const eqArrays = function(firstArray, secondArray) {
-  // Compare lengths of both arrays
-  if (firstArray.length !== secondArray.length) return false;
-  // If they are the same length, loop through them and see if each of their indexes match up as well.
-  for (let i = 0; i < firstArray.length; i++) {
-    if (firstArray[i] !== secondArray[i]) {
-    //If they don't match, return false
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual, expected)) {
@@ -21,4 +10,4 @@ const assertArraysEqual = function(actual, expected) {
   }
 };
 
-assertArraysEqual();
+module.exports = assertArraysEqual;
